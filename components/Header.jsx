8542619dd8aa4b57c8ddad30426ad3908b5859a0
anna leme <annaleme.tds1.senai@gmail.com>
2025-03-27 10:23:40 -0300

    feat: header and init css

---
+++ components/Header.jsx
@@ -0,0 +1,11 @@
+import styles from "../styles/Header.module.css";
+
+export default function Header() {
+    return (
+        <header className={styles.header}>
+            <div className={styles.profile}>
+                <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fbr.freepik.com%2Ffotos%2Famigas-juntas%2F3&psig=AOvVaw0azlAPN_R62X0JsOw95_9A&ust=1743166196594000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCIijn8amqowDFQAAAAAdAAAAABAE"></img>
+            </div>
+        </header>
+    )
+}
