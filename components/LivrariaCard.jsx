8341ab35f85a73e533ca8f81cbdbe3309b54beb0
maria parma <maria.parma@aluno.senai.br>
2025-04-03 13:35:09 -0300

    fix: update LivrariaCard.jsx

--- components/LivrariaCard.jsx
+++ components/LivrariaCard.jsx
@@ -1,10 +1,10 @@
 import styles from "../styles/LivrariaCard.module.css";
 
-export default function LivrariaCard ({ name, image, link, className}) {
+export default function LivrariaCard ({ name, link, className}) {
     return (
         <li className={`${styles.card}${className || ""}`}>
             <a href={link} target="_blank" rel="noopener noreferrer">
-                <img src= {image} alt={`Imagem do livro ${name}`}/>
+              <h2>teste</h2>
                 <p>{name}</p>
             </a>
         </li>
