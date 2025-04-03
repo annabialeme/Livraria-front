893bbe385b9a5e7ffcdf4439629b61e4dd966495
maria parma <mariaparma.tds1.senai@gmail.com>
2025-04-03 11:48:56 -0300

    fix: update page.jsx  and section.jsx

--- components/Section.jsx
+++ components/Section.jsx
@@ -1,15 +1,18 @@
 import styles from  "../styles/Section.module.css";
 import LivrariaCard from "../components/LivrariaCard";
+import { livros } from "../data/livros"
+import { autores } from "../data/autores";
+import { socialList } from "../data/socialList";
 
-export default function Section ({ title, description, data, cardClassName}) {
+export default function Section ({ title, description, data }) {
     return (
         <section className={styles.section}>
             <div className={styles.content}>
                 <h2>{title}</h2>
                 <p>{description}</p>
                 <ul className={styles.list} >
-                {Array.isArray(data) && data.map((item) => (
-                <LivrariaCard key={item.name} {...item} className={cardClassName} />
+                { data.map((item) => (
+                <LivrariaCard key={item.name} {...item}  />
                     ))}
                 </ul>
             </div>
