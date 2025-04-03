a28b6564a95490964bde5e173a38b0c951338960
maria parma <mariaparma.tds1.senai@gmail.com>
2025-04-03 11:17:23 -0300

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,3 @@
-import Header from "../components/Header";
 import Header from "../components/Header"; 
 import Section from "../components/Section"; 
 import SocialCard from "../components/LivrariaCard"; 
