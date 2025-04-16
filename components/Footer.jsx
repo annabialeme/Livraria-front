08929a55f2738a993439745afa20ddd1b3d6ed0e
anna leme <annaleme.tds1.senai@gmail.com>
2025-04-16 14:41:13 -0300

    fix: footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,8 +2,33 @@ import styles from "../styles/Footer.module.css";
 
 export default function Footer() {
   return (
-    <div className={styles.tabela}>
-     <Section title ="BookVerse" description="Livros que lançaram recentemente" data={lançamentos}cardClassName ={styles.roundedCard} />
-    </div>
+    <footer className={styles.footer}>
+      <div className={styles.section}>
+        <h3>BookVerse</h3>
+        <p>Onde histórias ganham vida</p>
+        <p>A BookVerse é uma livraria online que conecta leitores ávidos com os melhores e mias diversos</p>
+      </div>
+      <div className={styles.footer-section}>
+        <h3> Links Rápidos</h3>
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#catalogo">Catálogo</a></li>
+          <li><a href="#ofertas">Ofertas</a></li>
+          <li><a href="#sobre-nos">Sobre Nós</a></li>
+          <li><a href="#contato">Contato</a></li>
+        </ul>
+      </div>
+
+      <div className={styles.footer-section}>
+        <h3>Categorias</h3>
+        <ul>
+          <li><a href="#ficcao-cientifica">Ficção Científica</a></li>
+          <li><a href="#romance">Romance</a></li>
+          <li><a href="#tecnologia">Tecnologia</a></li>
+          <li><a href="#fantasia">Fantasia</a></li>
+          <li><a href="#historia">História</a></li>
+        </ul>
+      </div>
+    </footer>
   )
 }
