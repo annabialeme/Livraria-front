d6c5705b5abce8cfd1002b9418098f3c1499465f
maria parma <mariaparma.tds1.senai@gmail.com>
2025-04-17 14:14:39 -0300

    fix: update header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,17 +1,16 @@
 import React from "react";
-
-import styles from "../styles/Header.module.css"
+import styles from "../styles/Header.module.css";
 
 export default function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.logo}>BookVerse📚</div>
       <nav>
-        <ul>
+        <ul className={styles.navList}>
           <li><a href="#inicio">Início</a></li>
           <li><a href="#livros">Livros</a></li>
           <li><a href="#categorias">Categorias</a></li>
-          <li><a href="#autotes">Autores</a></li>
+          <li><a href="#autores">Autores</a></li>
           <li><a href="#ofertas">Ofertas</a></li>
         </ul>
       </nav>
@@ -22,5 +21,5 @@ export default function Header() {
         <span className="profile">👨🏻‍🦲</span>
       </div>
     </header>
-  )
+  );
 }
